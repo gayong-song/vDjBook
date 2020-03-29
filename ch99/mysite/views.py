f221d87c0d1fd62b354a92c98185a20d916dbe94
gayong-song <sgygayoung@gmail.com>
2020-03-30 00:19:03 +0900

    Fix bug
    Solve 403 PermissionDenied error

--- ch99/mysite/views.py
+++ ch99/mysite/views.py
@@ -4,6 +4,7 @@ from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
 
 from django.contrib.auth.mixins import AccessMixin
+from django.views.defaults import permission_denied
 
 class HomeView(TemplateView):
     template_name = 'home.html'
@@ -19,9 +20,8 @@ class UserCreateDoneTV(TemplateView):
 class OwnerOnlyMixin(AccessMixin):
     raise_exception = True
     permission_denied_message = "Owner only can update/delete the object"
-
-def dispatch(self, request, *args, **kwargs):
-    obj = self.get_object()
-    if request.uesr != obj.owner:
-        return self.handle_no_permission()
-    return super().dispatch(request, *args, **kwargs)
+    def dispatch(self, request, *args, **kwargs):
+        obj = self.get_object()
+        if request.user != obj.owner:
+            return self.handle_no_permission()
+        return super().dispatch(request, *args, **kwargs)

6695666391ec0ebcc528b1e1f34e0c6d54036f0f
gayong-song <sgygayoung@gmail.com>
2020-04-09 21:20:54 +0900

    Chapter 12
    - add/change functionality on photo application

--- ch99/photo/models.py
+++ ch99/photo/models.py
@@ -4,8 +4,10 @@ from django.urls import reverse
 from photo.fields import ThumbnailImageField
 
 class Album(models.Model):
-    name = models.CharField(max_length=30)
+    name = models.CharField('NAME', max_length=30)
     description = models.CharField('One Line Description', max_length=100, blank=True)
+    owner = models.ForeignKey('auth.User', on_delete=models.CASCADE, verbose_name='OWNER',
+            blank=True, null=True)
 
     class Meta:
         ordering = ('name',)
@@ -20,8 +22,9 @@ class Photo(models.Model):
     album = models.ForeignKey(Album, on_delete=models.CASCADE)
     title = models.CharField('TITLE', max_length=30)
     description = models.TextField('Photo Description', blank=True)
-    image = ThumbnailImageField(upload_to='photo/%Y/%m')
-    upload_dt = models.DateTimeField('Upload Date', auto_now_add=True)
+    image = ThumbnailImageField('IMAGE', upload_to='photo/%Y/%m')
+    upload_dt = models.DateTimeField('UPLOAD DATE', auto_now_add=True)
+    owner = models.ForeignKey('auth.User', on_delete=models.CASCADE, verbose_name='OWNER', blank=True, null=True)
 
     class Meta:
         ordering = ('title',)
